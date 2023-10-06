4ba6ad376258de8b6d4d4e446c5371c418c4cd08
Pinerealm <osalami09@gmail.com>
2023-10-06 07:51:15 +0300

    Annotate the make_multiplier function

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Complex types - functions"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """Returns a function that multiplies a float by multiplier
+    """
+    def multiply_by_multiplier(n: float) -> float:
+        """Returns the product of a float and multiplier
+        """
+        return n * multiplier
+    return multiply_by_multiplier
