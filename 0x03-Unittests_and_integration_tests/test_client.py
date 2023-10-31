30b6b8dab4735b1232b85403154bac11603e4e24
Pinerealm <osalami09@gmail.com>
2023-10-31 12:11:05 +0300

    Test the _public_repos_url using PropertyMock

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -3,13 +3,14 @@
 from client import GithubOrgClient
 from parameterized import parameterized
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 
 
 class TestGithubOrgClient(unittest.TestCase):
     """The TestGithubOrgClient class
     """
-    param = {"payload": True}
+    param = {"payload": True,
+             "repos_url": "http://test.com"}
 
     @parameterized.expand([
         ("google",),
@@ -24,3 +25,13 @@ class TestGithubOrgClient(unittest.TestCase):
         self.assertEqual(test_client.org, self.param)
         mock_get_json.assert_called_once_with(
             f"https://api.github.com/orgs/{test_org_name}")
+
+    def test_public_repos_url(self):
+        """Test the _public_repos_url method
+        """
+        with patch('client.GithubOrgClient.org',
+                   new_callable=PropertyMock) as mock_org:
+            mock_org.return_value = self.param
+            test_client = GithubOrgClient("test")
+            self.assertEqual(test_client._public_repos_url,
+                             self.param["repos_url"])
