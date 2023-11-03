2aed06844cb59ba53c8f57102524100690d50b5c
Pinerealm <osalami09@gmail.com>
2023-11-03 15:19:16 +0300

    Add unit test for the public_repos method

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -11,6 +11,15 @@ class TestGithubOrgClient(unittest.TestCase):
     """
     param = {"payload": True,
              "repos_url": "http://test.com"}
+    repos_payload = [{"id": 12345, "node_id": "ertghbjn", "name": "test",
+                      "full_name": 'test/test', "private": False,
+                      "license": {"key": "apache-2.0", "name": "Apache 2.0"}},
+                     {"id": 54321, "node_id": "yuiop", "name": "test2",
+                      "full_name": 'test/test2', "private": False,
+                      "license": {"key": "mit", "name": "MIT License"}},
+                     {"id": 98765, "node_id": "dfghjk", "name": "test3",
+                      "full_name": 'test/test3', "private": False,
+                      "license": None}]
 
     @parameterized.expand([
         ("google",),
@@ -35,3 +44,19 @@ class TestGithubOrgClient(unittest.TestCase):
             test_client = GithubOrgClient("test")
             self.assertEqual(test_client._public_repos_url,
                              self.param["repos_url"])
+
+    @patch('client.get_json')
+    def test_public_repos(self, mock_get_json):
+        """Test the public_repos method"""
+        mock_get_json.return_value = self.repos_payload
+        with patch('client.GithubOrgClient._public_repos_url',
+                   new_callable=PropertyMock) as mock_public_repos_url:
+            mock_public_repos_url.return_value = self.param["repos_url"]
+            test_client = GithubOrgClient("test")
+            self.assertEqual(test_client.public_repos(), ["test", "test2",
+                                                          "test3"])
+            self.assertEqual(test_client.public_repos("apache-2.0"), ["test"])
+
+            self.assertEqual(test_client.public_repos("mit"), ["test2"])
+            mock_get_json.assert_called_once_with(self.param["repos_url"])
+            mock_public_repos_url.assert_called_once()
