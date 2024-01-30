6b956f98606aa83d6cc85ce1d351ce2b5ce513fd
Pinerealm <osalami09@gmail.com>
2024-01-30 13:47:29 +0300

    Add Optional type hint to default parameter in safely_get_value function

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """Using TypeVar"""
-from typing import Any, Mapping, TypeVar, Union
+from typing import Any, Mapping, TypeVar, Union, Optional
 T = TypeVar('T')
 
 
 def safely_get_value(dct: Mapping, key: Any,
-                     default: Union[T, None] = None) -> Union[Any, T]:
+                     default: Optional[T] = None) -> Union[Any, T]:
     """Get value from dict"""
     if key in dct:
         return dct[key]
